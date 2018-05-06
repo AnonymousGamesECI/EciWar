2a7aa8e935a266536eae59d19bbdb2c9abce7677
Juan David Ramirez Mendoza <juan.ramirez-me@mail.escuelaing.edu.co>
2018-05-06 13:43:45 -0500

    RabbitMQ config added

--- assets/Scripts/StompHandler.js
+++ assets/Scripts/StompHandler.js
@@ -1,11 +1,27 @@
 export function getStompClient() {
-	return new Promise((resolve) => {
+	//Local app configuration
+	/*return new Promise((resolve) => {
 		//const socket = new SockJS("http://localhost:8080//stompendpoint");
 		var socket = new SockJS("/stompendpoint");
 		var stompClient = Stomp.over(socket);
 		stompClient.connect({}, function(frame) {
 			resolve(stompClient);
 		});
+	});*/
+	
+	//Scalability app CloudAmqp configuration
+	return new Promise((resolve) => {
+		var socket = new SockJS("/stompendpoint");
+		var stompClient = Stomp.over(socket);
+		stompClient.connect("tkownfax", "UJ4YP9jVSniRrhGMVLdMev0EOY6EphFa",
+			function(frame) {
+				resolve(stompClient);
+			}
+			,
+			function(error){
+				console.info("error: " + error);
+			}
+			, "tkownfax");
 	});
 };
 
